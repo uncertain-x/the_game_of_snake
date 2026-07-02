3d4566aedbc88e399678c581375ed2bed1adb523
Vi <zhangv316@gmail.com>
2026-07-02 16:21:16 +0200

    added the vga controller and display function

---
+++ srcs/display/display_control.vhd
@@ -0,0 +1,181 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 27.06.2026 13:53:16
+-- Design Name: 
+-- Module Name: display_control - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL; 
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity display_control is
+--  Port ( );
+    Port (
+        clk : in std_logic;
+        reset : in std_logic;
+        
+        --temporary set for snake and food
+        
+        -- VGA physical interface
+        VGA_R : out std_logic_vector(3 downto 0);
+        VGA_G : out std_logic_vector(3 downto 0);
+        VGA_B : out std_logic_vector(3 downto 0);
+        VGA_H_SYNC : out std_logic;
+        VGA_V_SYNC : out std_logic
+    );
+end display_control;
+
+architecture Behavioral of display_control is
+
+    --add the vga component to verify whether it works.
+    component vga_control is
+        Port (
+            clk : in std_logic;
+            reset : in std_logic;
+            h_sync : out std_logic;
+            v_sync : out std_logic;
+            video_on : out std_logic;
+            v_blank : out std_logic;
+            pixel_x : out integer range 0 to 639;
+            pixel_y : out integer range 0 to 479
+        );
+    end component;
+    
+    --add the precise clock from wiz
+    component clk_wiz_0 is
+        Port (
+            clk_out1_25 : out std_logic;
+            reset : in std_logic;
+            locked : out std_logic;
+            clk_in1 : in std_logic
+        );
+    end component;
+    
+    --the total size of vga(640 x 480) divides into 20 x 15 grids,
+    --which each grid has 32 x 32 pixels.
+    constant grid_width : integer := 20;
+    constant grid_height : integer := 15;
+    constant single_grid_size : integer := 32;
+    
+    -- color definitions
+    -- the color of wall sets gray
+    -- the color of background sets black
+    -- the color of snake head sets yellow
+    -- the color of snake sets green
+    -- the color of food sets red
+    constant wall_color : std_logic_vector(11 downto 0) := x"888";
+    constant background_color : std_logic_vector(11 downto 0) := x"000";
+    constant snake_head_color : std_logic_vector(11 downto 0) := x"ff0";
+    constant snake_color : std_logic_vector(11 downto 0) := x"0f0";
+    constant food_color : std_logic_vector(11 downto 0) := x"f00";
+    
+    --connect with VGA
+    -- clk 25MHz for VGA display 25 x 640 x 480 = 60 Hz
+    signal vga_clk : std_logic := '0';
+    signal locked_sig : std_logic := '0';
+    signal vga_reset : std_logic := '0';
+    --signal clk_div_cnt : unsigned(1 downto 0) := "00";
+    --signal clk_div : std_logic := '0';
+    -- enable the output
+    signal w_video_on : std_logic;
+    signal w_v_blank : std_logic;
+    -- pixel coordinate. Should it be start from 640 x 480 or 20 x 15?
+    signal w_pixel_x : integer range 0 to 639;
+    signal w_pixel_y : integer range 0 to 479;
+    -- grid coordinate.
+    --signal grid_x : integer range 0 to 19;
+    --signal grid_y : integer range 0 to 14;
+    
+    -- adapting to the read delay
+    signal video_on_delay : std_logic := '0';
+    -- prepare for next RGB display to avoid glitches(unstable noise)
+    signal next_rgb : std_logic_vector(11 downto 0) := (others => '0');
+
+begin
+    
+    -- clock from precise clock_wiz
+    clk_wiz : clk_wiz_0
+        Port map (
+            clk_out1_25 => vga_clk,
+            reset => reset,
+            locked => locked_sig,
+            clk_in1 => clk
+        );
+
+    vga_reset <= reset or (not locked_sig);
+    
+    -- VGA timing
+    vga_ctrl_timing : vga_control
+        Port map (
+            clk => vga_clk,
+            reset => vga_reset,
+            h_sync => vga_h_sync,
+            v_sync => vga_v_sync,
+            video_on => w_video_on,
+            v_blank => w_v_blank,
+            pixel_x => w_pixel_x,
+            pixel_y => w_pixel_y  
+        );
+        
+        -- drawing logic
+        process(vga_clk)
+            --temporary varible for snake and food
+            variable food_x, food_y : integer := 9;
+            variable snake_head_x, snake_head_y : integer := 11;
+            variable snake_body_x : integer := 12;
+            variable snake_body_y : integer := 11;
+            variable grid_x, grid_y : integer;
+        begin
+            if rising_edge(vga_clk) then
+                video_on_delay <= w_video_on;
+                
+                grid_x := w_pixel_x /single_grid_size;
+                grid_y := w_pixel_y / single_grid_size;
+                
+                -- display the wall
+                if (grid_x = 0) or (grid_x = 19) or (grid_y = 0) or (grid_y = 14) then
+                    next_rgb <= wall_color;
+                -- display the inside wall, game area
+                -- scan the coordinate and display the snake and food
+                elsif (grid_x = food_x) and (grid_y = food_y) then
+                    next_rgb <= food_color;
+                elsif (grid_x = snake_head_x) and (grid_y = snake_head_y) then
+                    next_rgb <= snake_head_color;
+                elsif (grid_x = snake_body_x) and (grid_y = snake_body_y) then
+                    next_rgb <= snake_color;
+                else
+                    next_rgb <= background_color;
+                end if;
+            end if;
+        end process;
+
+        -- vga output mapping
+        VGA_R <= next_rgb(11 downto 8) when video_on_delay = '1' else "0000";
+        VGA_G <= next_rgb(7 downto 4) when video_on_delay = '1' else "0000";
+        VGA_B <= next_rgb(3 downto 0) when video_on_delay = '1' else "0000";
+ 
+end Behavioral;
--- srcs/display/vga_control.vhd
+++ srcs/display/vga_control.vhd
@@ -33,11 +33,69 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity vga_control is
 --  Port ( );
+    Port(
+        clk : in std_logic;
+        reset : in std_logic;
+
+        h_sync : out std_logic;
+        v_sync : out std_logic;
+        video_on : out std_logic;
+        v_blank : out std_logic;
+
+        pixel_x : out integer range 0 to 639;
+        pixel_y : out integer range 0 to 479
+    );
 end vga_control;
 
 architecture Behavioral of vga_control is
+    --specific parameter from datasheet of vga
+    constant H_DISPLAY : integer := 640;
+    constant H_FRONT_PORCH : integer := 16;
+    constant H_BACK_PORCH : integer := 48;
+    constant H_PULSE_WIDTH : integer := 96;
+    constant H_SYNC_PULSE : integer := 800;
 
+    constant V_DISPLAY : integer := 480;
+    constant V_FRONT_PORCH : integer := 10;
+    constant V_BACK_PORCH : integer := 29;
+    constant V_PULSE_WIDTH : integer := 2;
+    constant V_SYNC_PULSE : integer := 521;
+
+    signal H_CNT : integer range 0 to H_SYNC_PULSE - 1 := 0;
+    signal V_CNT : integer range 0 to V_SYNC_PULSE - 1 := 0;
 begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                H_CNT <= 0;
+                V_CNT <= 0;
+             else
+                if  H_CNT = H_SYNC_PULSE - 1 then
+                    H_CNT <= 0;
+                    if V_CNT = V_SYNC_PULSE - 1 then
+                        V_CNT <= 0;
+                    else
+                        V_CNT <= V_CNT + 1;
+                    end if;
+                else
+                    H_CNT <= H_CNT + 1;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    -- generate synchronous signal
+    h_sync <= '0' when (H_CNT >= H_DISPLAY + H_FRONT_PORCH) and (H_CNT < H_DISPLAY + H_FRONT_PORCH + H_PULSE_WIDTH) else '1';
+    v_sync <= '0' when (V_CNT >= V_DISPLAY + V_FRONT_PORCH) and (V_CNT < V_DISPLAY + V_FRONT_PORCH + V_PULSE_WIDTH) else '1';
+
+    -- output signal condition
+    video_on <= '1' when (H_CNT < H_DISPLAY) and (V_CNT < V_DISPLAY) else '0';
+    v_blank <= '1' when (V_CNT >= V_DISPLAY) else '0';
+
+    -- output pixel coordinate
+    pixel_x <= H_CNT when (H_CNT < H_DISPLAY) else 0;
+    pixel_y <= V_CNT when (V_CNT < V_DISPLAY) else 0;
 
 
 end Behavioral;
